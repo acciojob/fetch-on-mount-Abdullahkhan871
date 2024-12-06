79734a11808006658deb2bfed13871bda3b4f392
Abdullah khan <akhan283785@gmail.com>
2024-12-06 10:20:42 +0530

    try

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [showData, setShowData] = useState([]);
+
+  function handleData() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      // .then((response) => setShowData(response))
+      .then((response) => response.json())
+      .then((response) => setShowData(response))
+      .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    handleData();
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {showData.length > 0 &&
+        showData.map((item) => (
+          <div key={item.id}>
+            <h2>
+              {item.id}. {item.title}
+            </h2>
+            <p>{item.body}</p>
+          </div>
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
